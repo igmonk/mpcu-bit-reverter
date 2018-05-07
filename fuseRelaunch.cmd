39463f6c257c34ed84ada1e78ee5a650092522ba
unknown <imoiseyenko93@gmail.com>
2018-05-08 02:24:53 +0300

    ROM Stack component another stuff.

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/Users/igor/Projects/mpcu_bit_reverter/TOP_GPR_TB_isim_beh.exe" -prj "C:/Users/igor/Projects/mpcu_bit_reverter/TOP_GPR_TB_beh.prj" "work.TOP_GPR_TB" 
+-intstyle "ise" -incremental -o "C:/Users/igor/Projects/mpcu_bit_reverter/MROM_ST_TB_isim_beh.exe" -prj "C:/Users/igor/Projects/mpcu_bit_reverter/MROM_ST_TB_beh.prj" "work.MROM_ST_TB" 
